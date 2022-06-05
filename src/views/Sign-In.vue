b56e26cabc08599c62115f6913bc14adc9fb7dd5
zx25 <timwells018@gmail.com>
2022-06-05 21:05:02 +0100

    tweaks

--- src/views/Sign-In.vue
+++ src/views/Sign-In.vue
@@ -1,18 +1,10 @@
-<!-- 
-	This is the sign in page, it uses the dashboard layout in: 
-	"./layouts/Default.vue" .
- -->
-
 <template>
-	<div class="sign-in">
-		
+	<div class="sign-in">	
 		<a-row type="flex" :gutter="[24,24]" justify="space-around" align="middle">
-
 			<!-- Sign In Form Column -->
 			<a-col :span="24" :md="12" :lg="{span: 12, offset: 0}" :xl="{span: 6, offset: 2}" class="col-form">
 				<h1 class="mb-15">Sign In</h1>
 				<h5 class="font-regular text-muted">Enter your email and password to sign in</h5>
-
 				<!-- Sign In Form -->
 				<a-form
 					id="components-form-demo-normal-login"
@@ -45,7 +37,6 @@
 					</a-form-item>
 				</a-form>
 				<!-- / Sign In Form -->
-
 				<p class="font-semibold text-muted">Don't have an account? <router-link to="/sign-in" class="font-bold text-dark">Sign Up</router-link></p>
 			</a-col>
 			<!-- / Sign In Form Column -->
@@ -55,9 +46,7 @@
 				<img src="images/img-signin.jpg" alt="">
 			</a-col>
 			<!-- Sign In Image Column -->
-
 		</a-row>
-		
 	</div>
 </template>
 
