27a3e49da6b6a011521f5d564ee1a6e418a73eb9
zx25 <timwells018@gmail.com>
2022-06-08 10:50:57 +0100

    Commit

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import DefaultLayout from './layouts/Default.vue'
 import DashboardLayout from './layouts/Dashboard.vue'
 import DashboardRTLLayout from './layouts/DashboardRTL.vue'
+import GoogleTrend from './components/GoogleTrend/GoogleTrend.vue'
 import router from './router'
 import store from "@/store";
 
@@ -19,6 +20,9 @@ Vue.config.productionTip = false
 Vue.component("layout-default", DefaultLayout);
 Vue.component("layout-dashboard", DashboardLayout);
 Vue.component("layout-dashboard-rtl", DashboardRTLLayout);
+
+Vue.component('google-trend',GoogleTrend);
+
 
 new Vue({
   store,
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,15 @@
 <template>
 	<div>
+  		<google-trend id="google-trend" keywords="[btc]" geo="US" search="2020-01-01 2022-06-01"/>
+  		<google-trend id="google-trend" keywords="[recession]" geo="US" search="2020-01-01 2022-06-01"/>
+
 		<!-- Markets -->
 		<a-row :gutter="24" type="flex" align="stretch">
 			<a-col :span="24" :lg="12" :xl="12" class="mb-24" v-for="(market, index) in markets" :key="index">
 				<CardChartInfo :title="market.title" :ticker="market.ticker"></CardChartInfo>
 			</a-col>
 		</a-row>
+
 	</div>
 </template>
 
@@ -30,6 +34,12 @@ export default ({
     	this.$store.dispatch("quote/getQuote");
 	}
 })
+
+
+
+
+
+
 
 </script>
 
