c3f44dc3c8329246b9fbbc2bd83aa66605266564
zx25 <timwells018@gmail.com>
2022-10-22 22:32:26 +0100

    Row colouring

--- src/views/StockWatch.vue
+++ src/views/StockWatch.vue
@@ -6,32 +6,34 @@
 				:data-source="stockWatches" 
 				:rowKey="(record,index) => index"
 				@expand="onExpand"
+				:rowClassName="rowColor"
 				@expandedRowsChange="expandedRowsChange"
+				size="small"
 				class='table table-small' style="margin: 0; background-color: white;">
 
 				<template slot="expandedRowRender" slot-scope="record" style="margin: 0">
 					<a-row :gutter="24" type="flex">
-						<a-col :span="18">
+						<a-col :span="24">
 							<WidgetTradingView :symbol="fullSymbol(record.n)" @container="container"></WidgetTradingView>
 						</a-col>						
-						<a-col :span="6">
-							<!--CardTraderBrokerInfo :symbol="fullSymbol(record.epic)"></CardTraderBrokerInfo-->
-						</a-col>						
+						<!--a-col :span="6">
+							<CardTraderBrokerInfo :symbol="fullSymbol(record.epic)"></CardTraderBrokerInfo>
+						</a-col-->						
 					</a-row>
 				</template>
 
 				<template slot="epic" slot-scope="epic">
-					<p class="m-0 font-regular text-muted">{{ epic }}</p></template>
+					<p class="m-0">{{ epic }}</p></template>
 				<template slot="price" slot-scope="price">
-					<p class="m-0 font-regular text-muted">{{ price }}</p></template>				
+					<p class="m-0">{{ price }}</p></template>				
 				<template slot="buy" slot-scope="tp">
-					<p class="m-0 font-regular text-muted">{{ tp }}</p>
+					<p class="m-0">{{ tp }}</p>
 				</template>
 				<template slot="trigger" slot-scope="trigger">
-					<p class="m-0 font-regular text-muted">{{ trigger }}</p>
+					<p class="m-0">{{ trigger }}</p>
 				</template>
 				<template slot="timestamp" slot-scope="timestamp">
-					<p class="m-0 font-regular text-muted">{{ timestamp }}</p></template>	
+					<p class="m-0">{{timeStamp(timestamp)}}</p></template>	
 			</a-table>
 		</a-col>
 	</a-row>
@@ -54,7 +56,7 @@ const stockWatchColumns = [
 	{ title: 'Epic', dataIndex: 'n', scopedSlots: { customRender: 'epic' }},
 	{ title: 'Price', dataIndex: 'v', scopedSlots: { customRender: 'price' }},
 	{ title: 'Trigger', dataIndex: 'h', scopedSlots: { customRender: 'trigger' }},
-	{ title: 'Buy', dataIndex: 'tp',scopedSlots: { customRender: 'buy' }},
+	// { title: 'Buy', dataIndex: 'tp',scopedSlots: { customRender: 'buy' }},
 	{ title: 'TimeStamp', dataIndex: 'ts',scopedSlots: { customRender: 'timestamp' }}
 ];
 const epicCorrections = [{in:"T17",out:"TM17"}]
@@ -72,6 +74,7 @@ export default ({
 	data() {
 		return {
 			stockWatchColumns,
+			/*
 			pagination: { 
 				pageSize: 10, onChange: (p) => {
 					for(let i=0; i < this.expandedIdList.length; i++) {
@@ -80,7 +83,7 @@ export default ({
 					}
 					this.expandedIdList = []
 				},
-			},
+			},*/
 			expandedIdList: [],
 			expandedRowKeys: []
 		}
@@ -92,6 +95,19 @@ export default ({
 			if(nEpic) return "LSE:" + nEpic.out; 
 			return "LSE:" + epic.split(".")[0]; 
 		},
+		expandedRowsChange(r) {
+			console.log("expandedRowsChange:",r)
+		},
+		onExpand(exp,r) { 
+			console.log("onExpand: ",exp,r);
+		},
+		rowColor(row) {
+			if(row.tp) return "tiggered"
+			return ""
+		},
+		timeStamp(ts) {
+			return ts.split("T")[0]
+		}
 	},	
 	mounted() {
 		this.$store.dispatch("stockwatch/getStockWatches");
@@ -99,4 +115,6 @@ export default ({
 })
 </script>
 
-<style lang="scss"></style>
+<style>
+.tiggered { background-color:rgb(174, 243, 174) }
+</style>
