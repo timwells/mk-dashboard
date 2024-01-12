3ce30dd6f71690843e79d15fb08736b4546849c1
Tim Wells <timwells018@gmail.com>
2024-01-12 12:13:06 +0000

    updates

--- functions/wscrape/digrin.js
+++ functions/wscrape/digrin.js
@@ -17,7 +17,7 @@ async function getPuppetInstance() {
     return gPuppetInstance
 }
 
-async function processRequest(url,timeout) {
+async function processRequest(url,tag, timeout) {
     initStopWatch()
     console.log(url)
     const _puppetInstance = await getPuppetInstance();
@@ -28,28 +28,42 @@ async function processRequest(url,timeout) {
     const htmlContent = await page.content();
 
     const $ = cheerio.load(htmlContent, { xmlMode: true, decodeEntities: false });
-    const viewSvg = []
-    let svgContainer = $('#price_chart');
+    const Svgs = []
+    let svgContainer = $(`#${tag}`);
     
+    const sampleSVG = `<svg version="1.1" height="250" width="250"><circle class="circle" cx="250" cy="250" r="250" stroke-width="1" fill="#3e8ed0" fill-opacity="0.8"/><circle class="circle" cx="250" cy="250" r="23" stroke-width="1" fill="black"/><circle class="circle" cx="250" cy="250" r="234" stroke-width="2" fill="#00d1b2"/></svg>`
     svgContainer.each(async (i, e) => {
         let chartHtml = $(e).html();
         const svgRegex = /<svg[\s\S]*?<\/svg>/;
         const svgMatch = chartHtml.match(svgRegex);
         const svgCleansed = svgMatch[0].replaceAll("\"","'")
-        // const buffer = Buffer.from(svgCleansed, 'utf-8');
-        // viewSvg.push("data:image/svg+xml;base64,"+buffer.toString('base64'))
-        viewSvg.push(svgCleansed)
+
+        //const buffer = Buffer.from(svgCleansed, 'utf-8');
+        //Svgs.push("data:image/svg+xml;base64,"+buffer.toString('base64'))
+
+        //const buffer = Buffer.from(sampleSVG.replaceAll("\"","'"), 'utf-8');
+        //Svgs.push("data:image/svg+xml;base64,"+buffer.toString('base64'))
+        //chartsSvgs.push("data:image/svg+xml;base64,"+buffer.toString('base64'))
+        Svgs.push(svgCleansed)
     });
 
     watchTimeNow('viewSvg',true);
-    return viewSvg
+    return Svgs
 }
 
 const price = async (req, res) => {
     // https://www.digrin.com/stocks/detail/PLUS.L/price
-    res.status(200).json(await processRequest(`${DGN_PRICE_SITE_URL}/stocks/detail/${req.query.epic}/price`,60000))
+    res.status(200).json(await processRequest(`${DGN_PRICE_SITE_URL}/stocks/detail/${req.query.epic}/price`,
+        "price_chart",60000))
+}
+
+const earnings = async (req, res) => {
+    // https://www.digrin.com/stocks/detail/KGH.L/earnings
+    res.status(200).json(await processRequest(`${DGN_PRICE_SITE_URL}/stocks/detail/${req.query.epic}/earnings`,
+        "quarterly_earnings",60000))
 }
 
 module.exports = {
     price,
+    earnings
 }
--- src/components/Cards/CardPriceInfo.vue
+++ src/components/Cards/CardPriceInfo.vue
@@ -1,15 +1,25 @@
 <template>
 	<a-card :bordered="false" class="card-info">
         <!--p>{{epic}}</p-->
-        <a-row>
+        <a-row v-if="dgnPriceModels.length>0">
             <a-col 
                 :span="12" 
                 :lg="12" 
                 :xl="12" 
                 class="mb-12" v-for="(dgnPriceModel, index) in dgnPriceModels" :key="index">
-                    <!--img :src="dgnPriceModel" width="600"-->
+                    <img :src="dgnPriceModel" width="600">
                     <p>{{ dgnPriceModel }}</p>
-            </a-col>			
+            </a-col>
+            <!--svg version="1.1"
+                baseProfile="full"
+                width="300"
+                height="200"
+                xmlns="http://www.w3.org/2000/svg">
+                <rect width="100%" height="100%" fill="black" />
+                <circle cx="150" cy="100" r="90" fill="blue" />
+            </svg-->
+            <!--img src="svg" width="300" height="200"/>
+            <object :data="svg"  width="300" height="200"></object-->
         </a-row>
 	</a-card>
 </template>
@@ -23,7 +33,12 @@ export default ({
     },
     computed: {...mapState("wscrape", ["dgnPriceModels"])},
     data() {
-        return {}
+        return {
+            svg: `<svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="100%" height="100%" fill="black" />
+                    <circle cx="150" cy="100" r="90" fill="blue" />
+                </svg>`
+        }
     },
     methods: {
     },
