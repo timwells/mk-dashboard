4fe90b775394191eeae7436769135f53736b448f
zx25 <timwells018@gmail.com>
2024-02-29 20:21:47 +0000

    updates

--- src/store/modules/app/app.js
+++ src/store/modules/app/app.js
@@ -14,6 +14,7 @@ const getters = {
   }
 }
 
+
 const mutations = {
   SET_SECRETS: (state, payload) => { state.secrets = payload },
   SET_CF_VERSION: (state, payload) => { state.cfversion = payload }
