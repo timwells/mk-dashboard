8ac269365041ad8c15e3bfbb4f1ac315889f295b
Tim Wells <timwells018@gmail.com>
2024-06-11 18:20:54 +0100

    ETF updates

--- functions/scrape-dev/hl/hl-etf-test.js
+++ functions/scrape-dev/hl/hl-etf-test.js
@@ -1,6 +1,9 @@
 (async () => {
     const hlServices = require('./hl-etf-services.js')
     await hlServices['listProviders']()
+    
+    // await hlServices['testDetail']()
+    
 
     console.log("done");
 })();
