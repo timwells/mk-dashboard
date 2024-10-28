ffeb8a048676e8cb7a5a1113c1870de685d24a59
Tim Wells <timwells018@gmail.com>
2024-10-28 17:10:37 +0000

    CoinView updates

--- src/store/modules/gbcc/gbcc.js
+++ src/store/modules/gbcc/gbcc.js
@@ -2,16 +2,17 @@ import axios from "axios";
 import {
   APP_CLOUD_FUNCTION_URL, 
   APP_FINTECH_HEADERS,
-  genericGet
 } from "../common/c.js"
 
 const state = {
   productsRefreshProgress: 0,
   productssRefreshComplete: true,
-  categories:[]
+  categories:[],
+  products:[]
 };
 
 const getters = {
+  gtProducts: (state) => (id) => state.products.find((e) => (e.tag === `products-${id}`).data),
 }
 
 const mutations = {
@@ -20,6 +21,8 @@ const mutations = {
     SET_PRODUCTS_REFRESH_PROGRESS: (state, payload) => (state.productsRefreshProgress = payload),
     SET_PRODUCTS_REFRESH_COMPLETE: (state, payload) => (state.productssRefreshComplete = payload),
 
+    ADD_PRODUCTS_CACHE: (state, payload) => (state.products = [...state.products, payload]),
+    SET_PRODUCTS_CACHE: (state, payload) => (state.products = payload),
 };
 
 const actions = {
@@ -33,104 +36,32 @@ const actions = {
     commit("SET_PRODUCTS_REFRESH_COMPLETE", false)
     const resp = await axios.get(`${APP_CLOUD_FUNCTION_URL}/gbcc/categories`,{ headers: APP_FINTECH_HEADERS })
     const totalCategories = resp.data.data.length;
-
-    console.log("totalCategories:",totalCategories)
     for(let category = 0; category < totalCategories; category++) {
       const resource = `${APP_CLOUD_FUNCTION_URL}/gbcc/products?id=${resp.data.data[category].category}` 
       const { data } = await axios.get(resource, { headers: APP_FINTECH_HEADERS })
-      
-      // console.log(category,totalCategories,(category+1)/totalCategories,(100*((category+1)/totalCategories)))
-
       commit("SET_PRODUCTS_REFRESH_PROGRESS",+(100*((category+1)/totalCategories)).toFixed(0))
-
     }
     commit("SET_PRODUCTS_REFRESH_COMPLETE", true)
   },
 
- /*
-  async listCacheFiles({commit}) {
-    const {data} = await axios.get(
-        `${APP_CLOUD_FUNCTION_URL}/hl/funds/pages/list-objs`, 
-            { headers: APP_FINTECH_HEADERS })
-    commit("SET_FUNDS_CACHE_LIST", data)
-  },
-*/
-/*
-  async getFunds({commit}) {
-    commit("SET_FUNDS_OBJ", null)
-    const {data} = await axios.get(
-        `${APP_CLOUD_FUNCTION_URL}/hl/funds`, 
-            { headers: APP_FINTECH_HEADERS })
-    commit("SET_FUNDS_OBJ", data)
-  },
-*/
-/*
-  async getFundDetails({commit},{companyid,sectorid,sedol}) {
-    const { data } = await axios.get(
-        `${APP_CLOUD_FUNCTION_URL}/hl/fund/details?companyid=${companyid}&sectorid=${sectorid}&sedol=${sedol}`, 
-            { headers: APP_FINTECH_HEADERS })
-    commit("ADD_FUND_DETAILS", data)
-  },
-*/
-/*
-  async getFundAnalysis({commit},{sedol}) {
-    const { data } = await axios.get(
-        `${APP_CLOUD_FUNCTION_URL}/hl/fund/analysis?sedol=${sedol}`, 
-            { headers: APP_FINTECH_HEADERS })
-    commit("ADD_FUND_ANALYSIS", data)
-  },
-*/
-/*
-  async getEtfsStats({ commit }) {
-    const { data } = await axios.get(`${APP_CLOUD_FUNCTION_URL}/hl/funds/stats`, 
-                                        { headers: APP_FINTECH_HEADERS })
-    commit("SET_FUNDS_STATS", data)
-  },
-*/
-/*
-  async getEtfsCompanies({commit}) {
-    commit("SET_ETFS_COMPANIES", [])
-    const { data } = await axios.get(`${APP_CLOUD_FUNCTION_URL}/hl/etfs/compaines/list`,{ headers: APP_FINTECH_HEADERS })
-    commit("SET_ETFS_COMPANIES", data)
-  },
-*/
-/*
-  async refreshEtfs({ commit }) {
-    commit("SET_ETFS_REFRESH_PROGRESS", 0)
-    commit("SET_ETFS_REFRESH_COMPLETE", false)
-    commit("SET_ETFS_OBJ", null)
+  async getProducts({commit},{category}) {
+    const resource = `${APP_CLOUD_FUNCTION_URL}/gbcc/products?id=${category}`
+    console.log(resource)
+    const { data } = await axios.get(resource, { headers: APP_FINTECH_HEADERS })
+    commit("ADD_PRODUCTS_CACHE", data)
 
-    // Request Number of Companies
-    let totalCompanies = 0
-    const resp = await axios.get(`${APP_CLOUD_FUNCTION_URL}/hl/etfs/compaines/list`,{ headers: APP_FINTECH_HEADERS })
-    totalCompanies = resp.data.length
+    // commit("SET_PRODUCTS_CACHE", [])
+    //const resp = await axios.get(`${APP_CLOUD_FUNCTION_URL}/gbcc/categories`,{ headers: APP_FINTECH_HEADERS })
+    //const totalCategories = resp.data.data.length;
+    
+    //for(let category = 0; category < totalCategories; category++) {
+      //const resource = `${APP_CLOUD_FUNCTION_URL}/gbcc/products?id=${resp.data.data[category].category}` 
+      //const { data } = await axios.get(resource, { headers: APP_FINTECH_HEADERS })
+      //commit("ADD_PRODUCTS_CACHE", data)
+    //}
 
-    for(let company = 0; company < totalCompanies; company++) {
-      const resource = `${APP_CLOUD_FUNCTION_URL}/hl/etfs/compaines/funds/list?companyid=${resp.data[company].id}`  // /etfs/compaines/funds/list'
-      console.log(resource)
-      const { data } = await axios.get(resource, { headers: APP_FINTECH_HEADERS })
-      console.log(data)
-      commit("SET_ETFS_REFRESH_PROGRESS",Math.ceil(+(100*((company)/totalCompanies)).toFixed(0)))
-      // await new Promise((s) => setTimeout(s, 1000));
-    }
-    commit("SET_ETFS_REFRESH_COMPLETE", true)
-  },
-*/
-/*
-  async getEtfs({commit}) {
-    commit("SET_ETFS_OBJ", {})
-    const { data } = await axios.get(`${APP_CLOUD_FUNCTION_URL}/hl/etfs`,{ headers: APP_FINTECH_HEADERS })
-    commit("SET_ETFS_OBJ", data)
-  },
-  async getEtfDetails({commit},{sedol}) {   
-    const { data } = await axios.get(
-                        `${APP_CLOUD_FUNCTION_URL}/hl/etf/details?sedol=${sedol}`, 
-                             { headers: APP_FINTECH_HEADERS })
-    console.log("getEtfDetails:",sedol,data)
 
-    commit("ADD_ETF_DETAILS", data)
   },
-*/
 }
 
 export default {
--- src/views/CoinsView.vue
+++ src/views/CoinsView.vue
@@ -1,46 +1,56 @@
 <template>
-	<a-tabs default-active-key="1">
-		<a-tab-pane key="1" tab="Coins">
-			<a-card :bordered="false" class="header-solid h-full" :bodyStyle="{padding: 8}">
-				<a-row :gutter="24" type="flex">
-					<a-col :span="4">
-					</a-col>
-					<a-col :span="4">
-					</a-col>
-					<a-col :span="4">
-					</a-col>
-					<a-col :span="4">
-						<a-button @click="refreshProducts()" :disabled="isDisabled">Refresh Products</a-button>
-					</a-col>
-					<a-col :span="8">
-						<a-progress type="circle" :percent="productsRefreshProgress" :width="60" />
-					</a-col>
-				</a-row>
-			</a-card>
-		</a-tab-pane>
-		<a-tab-pane key="2" tab="?">
-		</a-tab-pane>
-	</a-tabs>
+	<div>
+		<a-card :bordered="false" class="header-solid h-full" :bodyStyle="{padding: 8}">
+			<a-row :gutter="24" type="flex">
+				<a-col :span="4">
+				</a-col>
+				<a-col :span="4">
+				</a-col>
+				<a-col :span="4">
+				</a-col>
+				<a-col :span="4">
+					<a-button @click="refreshProducts()" :disabled="isDisabled">Refresh Products</a-button>
+				</a-col>
+				<a-col :span="8">
+					<a-progress type="circle" :percent="productsRefreshProgress" :width="60" />
+				</a-col>
+			</a-row>
+		</a-card>
+		<a-tabs v-model="activeKey">
+			<a-tab-pane v-for="(category,i) in categories.data" :key="i" :tab="category.name">
+				<!--pre>{{ category}}</pre-->
+				<!--pre>{{ gtProducts(category.category) }}</pre-->
+				<pre>{{ products }}</pre>
+			</a-tab-pane>
+		</a-tabs>
+		<pre>{{ products }}</pre>
+	</div>
 </template>
 
 <script>
+import { mapState, mapGetters } from "vuex";
 
-import { mapState } from "vuex";
 export default ({
 	components: {
 	},
 	computed: {
-		...mapState("gbcc", ["categories","productsRefreshProgress","productsRefreshComplete"]),	
+		...mapState("gbcc", ["categories","productsRefreshProgress","productsRefreshComplete","products"]),
+		...mapGetters("gbcc", ["gtProducts"]),
 	},
 	watch: {
-	productsRefreshComplete(n,o) {
-		if(n === true) {
-			this.isDisabled = false
+		productsRefreshComplete(n,o) {
+			if(n === true) {
+				this.isDisabled = false
+			}
+		},
+		categories(n,o) {
+			for(let i=0; i < n.data.length; i++)
+				this.$store.dispatch("gbcc/getProducts",{category: n.data[i].category});
 		}
-	},
 	},
 	data() {
 		return {
+			activeKey: 0,
 			isDisabled: false,
 		}
 	},
@@ -51,7 +61,7 @@ export default ({
 		}
 	},	
 	mounted() {
-		// this.$store.dispatch("gbcc/getCategories");
+		this.$store.dispatch("gbcc/getCategories");
 	}
 })
 </script>
